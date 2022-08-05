794183833c9635a41bb00c12ff8039e1ad3cab00
Mohammad Bagherinia <mohammad.bagherinia72@gmail.com>
2022-08-05 11:16:05 +0430

    Upgrade dependencies
    * Make it compatible for  Desktop & Android

--- desktop/src/com/badlogic/cubocy/desktop/DesktopLauncher.java
+++ desktop/src/com/badlogic/cubocy/desktop/DesktopLauncher.java
@@ -1,12 +1,15 @@
 package com.badlogic.cubocy.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import com.badlogic.cubocy.Cubocy;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Cubocy(), config);
-	}
+        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("Cubocy");
+        config.setWindowedMode(800, 480);
+        new Lwjgl3Application(new Cubocy(), config);
+    }
 }
